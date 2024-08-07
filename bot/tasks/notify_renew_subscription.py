0240c23a79efd31c9c29301bfae6e8a7baeb0c6d
maxmenkov <maxim.menkov@tutamail.com>
2024-08-07 15:51:17 +0300

    Send messages about subscription expiration today and tommorow.

--- bot/tasks/notify_renew_subscription.py
+++ bot/tasks/notify_renew_subscription.py
@@ -12,14 +12,16 @@ async def notify_users_to_renew_sub():
         return None
     
     for marzban_user in marzban_users_to_notify:
-        vpn_id = user['username']
+        vpn_id = marzban_user['username']
         user = await get_marzban_profile_by_vpn_id(vpn_id)
         if user is None:
             continue
         chat_member = await glv.bot.get_chat_member(user.tg_id, user.tg_id)
         if chat_member is None:
             continue
-        message = get_i18n_string("Hello, {name} 👋🏻\n\nThank you for using our service ❤️\n️\nYour VPN subscription expires at the end of the day tomorrow.\n️\nTo renew it, just go to the \"Join 🏄🏻‍♂️\" section and make a payment.", chat_member.user.language_code).format(name=chat_member.user.first_name)
+        message = get_i18n_string("Hello, {name} 👋🏻\n\nThank you for using our service ❤️\n\nYour VPN subscription expires {day}, at the end of the day.\n️\nTo renew it, just go to the \"Join 🏄🏻‍♂️\" section and make a payment.", chat_member.user.language_code).format(
+            name=chat_member.user.first_name,
+            day=get_expiration_day_str(user))
         await glv.bot.send_message(user.tg_id, message)
 
 async def get_marzban_users_to_notify():
@@ -29,7 +31,7 @@ async def get_marzban_users_to_notify():
     users = res['users']
     return filter(filter_users_to_notify, users)
 
-def filter_users_to_notify(user):
+def filter_users_to_notify(user, lang):
     user_expire_date = user['expire']
     if user_expire_date is None:
         return False
@@ -37,3 +39,8 @@ def filter_users_to_notify(user):
     now = int(time.time())
     after_tomorrow = now + 60 * 60 * 36
     return now < user_expire_date < after_tomorrow
+
+def get_expiration_day_str(user, ):
+    if user['expire'] < int(time.time()) + 60 * 60 * 12:
+        return get_i18n_string("today")
+    return get_i18n_string("tomorrow")
