c8655ce52009ed2e2e5f562084792e7b646c7b9a
ubuntu <maksim.yurievich.makarov@gmail.com>
2024-08-31 18:16:31 +0300

    Changed texts for stars pay button

--- bot/keyboards/xtr_pay.py
+++ bot/keyboards/xtr_pay.py
@@ -3,5 +3,5 @@ from aiogram.utils.i18n import gettext as _
 
 def get_xtr_pay_keyboard(price) -> InlineKeyboardBuilder:
     builder = InlineKeyboardBuilder()
-    builder.button(text=_('Pay {amount} ⭐️').format(amount=price), pay = True)
+    builder.button(text=_('Pay ⭐️'), pay = True)
     return builder.as_markup()
