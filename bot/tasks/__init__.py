0660c311d00bd09349523160f3617cf7983b3505
maxmenkov <maxim.menkov@tutamail.com>
2024-11-04 15:01:21 +0200

    Notify users about expired subscription.

--- bot/tasks/__init__.py
+++ bot/tasks/__init__.py
@@ -4,55 +4,17 @@ import logging
 
 from .update_token import update_token
 from .notify_renew_subscription import notify_users_to_renew_sub
+from .notify_expired_sub import notify_users_about_expired_sub
 
 import glv
 
 async def register():
-    logging.info('Starting scheduled \"Renew subscription\" and \"Update admin token\" crong jobs............\n\n\n......\nning............')
-    await asyncio.sleep(1)
-    logging.info('Running....\n')
-    await asyncio.sleep(1)
-    logging.info('Running.........\n')
-    await asyncio.sleep(1)
-    logging.info('Running.............................\n')
-    await asyncio.sleep(1)
-    logging.info('Running.................................................................\n')
-    await asyncio.sleep(1)
-    logging.info('ng.....................................................................................\n')
-    await asyncio.sleep(1)
-    logging.info('..................................................................................................\n')
-    await asyncio.sleep(1)
-    logging.info('..................................................................................................\n')
-    await asyncio.sleep(1)
-    logging.info('..................................................................................................\n')
-    await asyncio.sleep(1)
-    logging.info('..................................................................................................\n')
-    logging.info('..................................................................................................\n')
-    logging.info('..................................................................................................\n')
-    await asyncio.sleep(0.3)
-    logging.info('..................................................................................................\n')
-    logging.info('..................................................................................................\n')
-    logging.info('..................................................................................................\n')
-    logging.info('..................................................................................................\n')
-    logging.info('..................................................................................................\n')
-    await asyncio.sleep(0.3)
-    logging.info('..................................................................................................\n')
-    logging.info('..................................................................................................\n')
-    logging.info('..................................................................................................\n')
-    logging.info('..................................................................................................\n')
-    await asyncio.sleep(0.3)
-    logging.info('..................................................................................................\n')
-    logging.info('..................................................................................................\n')
-    logging.info('..................................................................................................\n')
-    logging.info('..................................................................................................\n')
-    await asyncio.sleep(0.3)
-    logging.info('..................................................................................................\n')
-    logging.info('..................................................................................................\n')
-    logging.info('..................................................................................................\n')
-    logging.info('....................................STARTED.......................................................\n')
-
+    logging.info('Register cron jobs.')
     aioschedule.every(5).minutes.do(update_token)
-    aioschedule.every().day.at(glv.config['RENEW_NOTIFICATION_TIME']).do(notify_users_to_renew_sub)
+    if (glv.config['RENEW_NOTIFICATION_TIME']):
+      aioschedule.every().day.at(glv.config['RENEW_NOTIFICATION_TIME']).do(notify_users_to_renew_sub)
+    if (glv.config['EXPIRED_NOTIFICATION_TIME']):
+      aioschedule.every().day.at(glv.config['EXPIRED_NOTIFICATION_TIME']).do(notify_users_about_expired_sub)
     while True:
         await aioschedule.run_pending()
         await asyncio.sleep(1)
