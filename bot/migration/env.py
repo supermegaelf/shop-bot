c1f67aa62e8451c65851f36ddacf367d8c59a542
maxmenkov <maxim.menkov@tutamail.com>
2024-11-17 16:35:17 +0200

    Add mariadb to docker compose. Add DB_PORT env.

--- bot/migration/env.py
+++ bot/migration/env.py
@@ -7,6 +7,7 @@ from sqlalchemy import pool
 from alembic import context
 
 from db.models import Base
+import glv
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -17,7 +18,7 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-config.set_main_option('sqlalchemy.url', f"mysql+pymysql://{os.environ.get('DB_USER')}:{os.environ.get('DB_PASS')}@{os.environ.get('DB_ADDRESS')}:3306/{os.environ.get('DB_NAME')}")
+config.set_main_option('sqlalchemy.url', f"{glv.config['DB_URL']}?async_fallback=True")
 target_metadata = Base.metadata
 
 # add your model's MetaData object here
