4d0e082b5e02cba03b1fce739514e398f72c2f6f
maxmenkov <maxim.menkov@tutamail.com>
2024-11-19 19:05:46 +0200

    Fix profile method.

--- bot/handlers/messages.py
+++ bot/handlers/messages.py
@@ -17,13 +17,8 @@ router = Router(name="messages-router")
 async def profile(message: Message):
     marzban_profile = await marzban_api.get_marzban_profile(message.from_user.id)
     trial_available = is_trial_available(message.from_user.id)
-    subscription_description = "Статус подписки: {status}\n\nОсталось дней: {days_left}\n\nТрафик: {data_used} GB/{data_limit} GB"
-    await message.answer(subscription_description.format(
-        status = marzban_profile['status'], 
-        days_left = (int(time.time()) - marzban_profile['expire'])//86400,
-        data_used = 0 if marzban_profile is None or marzban_profile['used_traffic'] == 0 else marzban_profile['used_traffic']//1073741824,
-        data_limit = 0 if marzban_profile is None or marzban_profile['data_limit'] == 0 else marzban_profile['data_limit']//1073741824
-        ), reply_markup=get_user_profile_keyboard(trial_available, glv.config['PANEL_GLOBAL'] + marzban_profile['subscription_url']))
+    message_text = get_profile_menu_string(marzban_profile)
+    await message.answer(message_text, reply_markup=get_user_profile_keyboard(trial_available, glv.config['PANEL_GLOBAL'] + marzban_profile['subscription_url'] if marzban_profile else ""))
     
 @router.message(F.text == __("Help 🕊"))
 async def help(message: Message):
