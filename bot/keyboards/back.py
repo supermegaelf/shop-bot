4c99e7fa84961e5dd152fb5d3ad8139c60aa4a73
maxmenkov <maxim.menkov@tutamail.com>
2024-11-20 15:21:38 +0200

    Fix back button

--- bot/keyboards/back.py
+++ bot/keyboards/back.py
@@ -4,8 +4,8 @@ from aiogram.utils.i18n import gettext as _
 def get_back_keyboard() -> InlineKeyboardMarkup:
     kb = [
         [
-            InlineKeyboardButton(text=_("⏪ Back"), callback_data='back'),
+            InlineKeyboardButton(text=_("⏪ Back"), callback_data='back')
         ]
     ]
     
-    return InlineKeyboardButton(keyboard=kb)
+    return InlineKeyboardMarkup(keyboard=kb)
