3c8fdfd8749f00676f874860786160ad2742abff
maxmenkov <maxim.menkov@tutamail.com>
2024-12-02 19:08:58 +0200

    Delete removed import

--- bot/keyboards/__init__.py
+++ bot/keyboards/__init__.py
@@ -7,4 +7,3 @@ from .xtr_pay import get_xtr_pay_keyboard
 from .help import get_help_keyboard, get_back_to_help_keyboard
 from .month import get_months_keyboard
 from .support import get_support_keyboard, get_reach_support_keyboard
-from .back import get_back_keyboard
