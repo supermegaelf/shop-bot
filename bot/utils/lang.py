040e3ffcb3f43dda692b2fa0d2472f1502bb5e8e
maxmenkov <maxim.menkov@tutamail.com>
2024-12-04 15:57:16 +0200

    Add logging

--- bot/utils/lang.py
+++ bot/utils/lang.py
@@ -12,7 +12,7 @@ def get_i18n_string(s, lang) -> str:
         logging.info("Getting translations from: " + str(Path(__file__).parent.parent / localedir))
         language_translations = gettext.translation(domain, Path(__file__).parent.parent / localedir, languages=[lang])
         language_translations.install()
-        
+        logging.info(gettext.gettext(s))
         return gettext.gettext(s)
     logging.info("Getting default translations from: localedir")
     language_translations = gettext.translation(domain, localedir, languages=['en'])
