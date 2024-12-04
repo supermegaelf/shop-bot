c7d5129980bcabe7fceaf96a877f2ff5b08ab08a
maxmenkov <maxim.menkov@tutamail.com>
2024-12-04 15:58:17 +0200

    Add logging

--- bot/utils/lang.py
+++ bot/utils/lang.py
@@ -3,6 +3,8 @@ from pathlib import Path
 
 import logging 
 
+_ = gettext.gettext
+
 domain = 'bot'
 localedir = 'locales'
 
@@ -12,10 +14,10 @@ def get_i18n_string(s, lang) -> str:
         logging.info("Getting translations from: " + str(Path(__file__).parent.parent / localedir))
         language_translations = gettext.translation(domain, Path(__file__).parent.parent / localedir, languages=[lang])
         language_translations.install()
-        logging.info(gettext.gettext(s))
-        return gettext.gettext(s)
+        logging.info(_(s))
+        return _(s)
     logging.info("Getting default translations from: localedir")
     language_translations = gettext.translation(domain, localedir, languages=['en'])
     language_translations.install()
     
-    return gettext.gettext(s)
+    return _(s)
