c2d5ee95d88caf2a324700995ffd1580be8d844b
maxmenkov <maxim.menkov@tutamail.com>
2024-12-04 16:02:55 +0200

    Remove debug logs

--- bot/utils/lang.py
+++ bot/utils/lang.py
@@ -9,12 +9,9 @@ localedir = 'locales'
 def get_i18n_string(s, lang) -> str:
     logging.info("lang: " + lang)
     if lang in ['ru']:
-        logging.info("Getting translations from: " + str(Path(__file__).parent.parent / localedir))
         language_translations = gettext.translation(domain, Path(__file__).parent.parent / localedir, languages=[lang])
         language_translations.install()
-        logging.info(_(s))
         return _(s)
-    logging.info("Getting default translations from: localedir")
     language_translations = gettext.translation(domain, localedir, languages=['en'])
     language_translations.install()
     
