f3f75b350d5e62c0100d7a801716d3f89e4da90f
maxmenkov <maxim.menkov@tutamail.com>
2025-01-07 17:10:22 +0200

    remove debug logs

--- bot/utils/lang.py
+++ bot/utils/lang.py
@@ -1,13 +1,10 @@
 import gettext 
 from pathlib import Path
 
-import logging 
-
 domain = 'bot'
 localedir = 'locales'
 
 def get_i18n_string(s, lang) -> str:
-    logging.info("lang: " + lang)
     if lang in ['ru']:
         language_translations = gettext.translation(domain, Path(__file__).parent.parent / localedir, languages=[lang])
         language_translations.install()
