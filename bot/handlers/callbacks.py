96fc8ede6c4d02446854a1cb4cadd4b12621c486
maxmenkov <maxim.menkov@tutamail.com>
2025-01-09 19:13:50 +0200

    Confirm tos

--- bot/handlers/callbacks.py
+++ bot/handlers/callbacks.py
@@ -172,6 +172,7 @@ async def callback_usage_problem(callback: CallbackQuery):
 async def callback_payment_method_select(callback: CallbackQuery):
     await callback.message.delete()
     good = goods.get(callback.data)
+    await callback.message.answer(text=_("message_confirm_tos"), reply_markup=get_payment_keyboard(good))
     await callback.message.answer(text=_("Select payment method ⬇️"), reply_markup=get_payment_keyboard(good))
     await callback.answer()
 
