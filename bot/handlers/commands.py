1adf1c44c4bdb5cc152167bb48169c923389af61
maxmenkov <maxim.menkov@tutamail.com>
2025-04-18 21:59:22 +0300

    Add promocode handler to start. Remove TLS from vless

--- bot/handlers/commands.py
+++ bot/handlers/commands.py
@@ -1,4 +1,5 @@
 import asyncio
+from datetime import datetime
 
 from aiogram import Router
 from aiogram import Dispatcher
@@ -10,6 +11,7 @@ from aiogram.utils.chat_action import ChatActionSender
 
 from keyboards import get_main_menu_keyboard
 from .messages import profile, help
+from db.methods import get_promo_code_by_code, has_activated_promo_code, activate_promo_code
 
 router = Router(name="commands-router") 
 
@@ -17,10 +19,29 @@ router = Router(name="commands-router")
     Command("start")
 )
 async def start(message: Message):
-    await message.answer(_("message_welcome").format(name=message.from_user.first_name))
-    async with ChatActionSender.typing(bot=message.bot, chat_id=message.chat.id):
-            await asyncio.sleep(1)
-    await message.answer(_("message_select_action"), reply_markup=get_main_menu_keyboard())
+    args = message.text.split()
+    tg_id = message.from_user.id
+    
+    if len(args) > 1 and args[1].startswith("promo_"):
+        promo_code = args[1].replace("promo_", "").upper()
+        promo = await get_promo_code_by_code(promo_code)
+        if not promo:
+            await message.answer(text=_("message_promo_not_found"), reply_markup=get_main_menu_keyboard())
+    
+        if promo.expires_at and promo.expires_at < datetime.now():
+            await message.answer(text=_("message_promo_expired"), reply_markup=get_main_menu_keyboard())
+    
+        if await has_activated_promo_code(tg_id, promo.id):
+            await message.answer(text=_("message_promo_already_activated"), reply_markup=get_main_menu_keyboard())
+    
+        await activate_promo_code(tg_id, promo.id)
+        await message.answer(text=_("message_promo_activated").format(discount=promo.discount_percent), 
+                         reply_markup=get_main_menu_keyboard())   
+    else:
+        await message.answer(_("message_welcome").format(name=message.from_user.first_name))
+        async with ChatActionSender.typing(bot=message.bot, chat_id=message.chat.id):
+                await asyncio.sleep(1)
+        await message.answer(_("message_select_action"), reply_markup=get_main_menu_keyboard())
 
 @router.message(
     Command("access")
