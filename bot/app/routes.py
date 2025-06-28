c98b92147e345387ba80333b03dee4e9af24ecc4
maxmenkov <maxim.menkov@tutamail.com>
2025-06-28 13:28:24 +0300

    notifications

--- bot/app/routes.py
+++ bot/app/routes.py
@@ -126,7 +126,7 @@ async def check_yookassa_payment(request: Request):
 async def notify_user(request: Request):
     secret = request.headers.get('x-webhook-secret') or request.headers.get('X-Remnawave-Signature')
     logging.info(secret)
-    request_data = request.json()
+    request_data = await request.json()
     logging.info(request_data)
     if secret != glv.config['WEBHOOK_SECRET']:
         return web.Response(status=403)
