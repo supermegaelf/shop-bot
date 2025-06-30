c54b34745cba1222790950c3cb8c4f066330ac92
maxmenkov <maxim.menkov@tutamail.com>
2025-06-30 14:31:25 +0300

    notifications

--- bot/tasks/__init__.py
+++ bot/tasks/__init__.py
@@ -6,7 +6,11 @@ from .update_token import update_marzban_token
 
 async def register_marzban_token_update_task():
     logging.info('Register update Marzban token task.')
-    aioschedule.every(5).minutes.do(update_marzban_token)
+    
+    async def wrapped_task():
+        await update_marzban_token()
+    
+    aioschedule.every(5).minutes.do(wrapped_task)
     while True:
         await aioschedule.run_pending()
         await asyncio.sleep(1)
