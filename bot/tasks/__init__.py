b6bc9fa19afa50d4fa6e457cdfff687c6bb80b70
supermegaelf <81654098+supermegaelf@users.noreply.github.com>
2025-07-28 12:22:39 +0300

    Update __init__.py

--- bot/tasks/__init__.py
+++ bot/tasks/__init__.py
@@ -12,9 +12,9 @@ async def register():
     logging.info('Register cron jobs.')
     aioschedule.every(5).minutes.do(update_token)
     if (glv.config['RENEW_NOTIFICATION_TIME']):
-      aioschedule.every().day.at(glv.config['RENEW_NOTIFICATION_TIME']).do(notify_users_to_renew_sub)
+        aioschedule.every().day.at(glv.config['RENEW_NOTIFICATION_TIME']).do(notify_users_to_renew_sub)
     if (glv.config['EXPIRED_NOTIFICATION_TIME']):
-      aioschedule.every().day.at(glv.config['EXPIRED_NOTIFICATION_TIME']).do(notify_users_about_expired_sub)
+        aioschedule.every().day.at(glv.config['EXPIRED_NOTIFICATION_TIME']).do(notify_users_about_expired_sub)
     while True:
         await aioschedule.run_pending()
         await asyncio.sleep(1)
