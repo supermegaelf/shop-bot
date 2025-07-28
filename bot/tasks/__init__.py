d88defdb13154a2619b0a7c749f4303648b0dc6a
supermegaelf <81654098+supermegaelf@users.noreply.github.com>
2025-07-28 13:14:41 +0300

    Update __init__.py

--- bot/tasks/__init__.py
+++ bot/tasks/__init__.py
@@ -2,19 +2,18 @@ import aioschedule
 import asyncio
 import logging
 
-from .update_token import update_token
-from .notify_renew_subscription import notify_users_to_renew_sub
-from .notify_expired_sub import notify_users_about_expired_sub
+from .update_token import update_marzban_token
 
 import glv
 
 async def register():
     logging.info('Register cron jobs.')
-    aioschedule.every(5).minutes.do(update_token)
-    if (glv.config['RENEW_NOTIFICATION_TIME']):
-        aioschedule.every().day.at(glv.config['RENEW_NOTIFICATION_TIME']).do(notify_users_to_renew_sub)
-    if (glv.config['EXPIRED_NOTIFICATION_TIME']):
-        aioschedule.every().day.at(glv.config['EXPIRED_NOTIFICATION_TIME']).do(notify_users_about_expired_sub)
+    
+    async def token_update_task():
+        await update_marzban_token()
+    
+    aioschedule.every(5).minutes.do(token_update_task)
+    
     while True:
         await aioschedule.run_pending()
         await asyncio.sleep(1)
