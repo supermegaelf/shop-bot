9f0b10239e7783d70e8e91e061db51bb6d723746
supermegaelf <maksim.yurievich.makarov@gmail.com>
2025-08-21 12:47:15 +0300

    add image support and disable link preview in broadcast

--- bot/handlers/broadcast.py
+++ bot/handlers/broadcast.py
@@ -1,7 +1,8 @@
 import asyncio
+from typing import Union
 
 from aiogram import Bot, Dispatcher, Router
-from aiogram.types import Message
+from aiogram.types import Message, PhotoSize, InputMediaPhoto
 from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
@@ -24,8 +25,23 @@ async def start_broadcast(message: Message, state: FSMContext):
 
 @router.message(BroadcastStates.waiting_for_message)
 async def process_message(message: Message, state: FSMContext):
-    await state.update_data(broadcast_message=message.text)
-    await message.answer(_("message_confirm_broadcast").format(text=message.text), reply_markup=get_confirmation_keyboard())
+    message_data = {
+        'text': message.text or message.caption or "",
+        'has_photo': bool(message.photo),
+        'photo_file_id': message.photo[-1].file_id if message.photo else None,
+        'message_type': 'photo' if message.photo else 'text'
+    }
+    
+    await state.update_data(broadcast_message=message_data)
+    
+    preview_text = message_data['text'] if message_data['text'] else "[Изображение без подписи]"
+    if message_data['has_photo']:
+        preview_text = f"[📷 Изображение]\n{preview_text}" if preview_text else "[📷 Изображение]"
+    
+    await message.answer(
+        _("message_confirm_broadcast").format(text=preview_text), 
+        reply_markup=get_confirmation_keyboard()
+    )
     await state.set_state(BroadcastStates.waiting_for_confirmation)
 
 @router.message(BroadcastStates.waiting_for_confirmation)
@@ -40,7 +56,7 @@ async def process_confirmation(message: Message, state: FSMContext, bot: Bot):
         return
 
     data = await state.get_data()
-    broadcast_message = data['broadcast_message']
+    broadcast_data = data['broadcast_message']
     
     await message.answer(_("message_broadcast_started"), reply_markup=get_main_menu_keyboard(lang=message.from_user.language_code))
     
@@ -51,7 +67,21 @@ async def process_confirmation(message: Message, state: FSMContext, bot: Bot):
     
     for user in users:
         try:
-            await bot.send_message(user.tg_id, broadcast_message)
+            if broadcast_data['message_type'] == 'photo':
+                await bot.send_photo(
+                    chat_id=user.tg_id,
+                    photo=broadcast_data['photo_file_id'],
+                    caption=broadcast_data['text'],
+                    parse_mode="HTML",
+                    disable_web_page_preview=True
+                )
+            else:
+                await bot.send_message(
+                    chat_id=user.tg_id, 
+                    text=broadcast_data['text'],
+                    parse_mode="HTML",
+                    disable_web_page_preview=True
+                )
             success_count += 1
             await asyncio.sleep(0.5)
         except Exception as e:
