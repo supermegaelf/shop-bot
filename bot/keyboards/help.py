4fd1e9b88040621dc09275cb289ecad21187bf19
root <root@instance131473.waicore.network>
2025-10-21 02:36:11 +0200

    Relocate help button to profile screen

--- bot/keyboards/help.py
+++ bot/keyboards/help.py
@@ -4,23 +4,23 @@ from aiogram.utils.i18n import gettext as _
 
 import glv
 
-def get_help_keyboard() -> InlineKeyboardMarkup:
+def get_help_keyboard(from_profile: bool = False) -> InlineKeyboardMarkup:
     builder = InlineKeyboardBuilder()
     builder.row(InlineKeyboardButton(text=_("button_support"), callback_data='support'))
     builder.row(InlineKeyboardButton(text=_("button_frequent_questions"), callback_data='faq'))
-    builder.row(InlineKeyboardButton(text=_("button_back"), callback_data='back_to_main'))
+    
+    if from_profile:
+        builder.row(InlineKeyboardButton(text=_("button_back"), callback_data='back_to_profile'))
+    else:
+        builder.row(InlineKeyboardButton(text=_("button_back"), callback_data='back_to_main'))
 
-    # if glv.config['RULES_LINK']:
-    #     builder.row(InlineKeyboardButton(text=_("button_terms_of_service"), url=glv.config['RULES_LINK']))
-    # else:
-    #     builder.row(InlineKeyboardButton(text=_("button_terms_of_service"), callback_data='tos')) 
-         
     return builder.as_markup()
 
-def get_back_to_help_keyboard() -> InlineKeyboardMarkup:
+def get_back_to_help_keyboard(from_profile: bool = False) -> InlineKeyboardMarkup:
+    callback_data = 'help_from_profile' if from_profile else 'help'
     kb = [
         [
-            InlineKeyboardButton(text=_("button_back"), callback_data='help')
+            InlineKeyboardButton(text=_("button_back"), callback_data=callback_data)
         ]
     ] 
     return InlineKeyboardMarkup(inline_keyboard=kb)
--- bot/keyboards/support.py
+++ bot/keyboards/support.py
@@ -3,8 +3,9 @@ from aiogram.utils.i18n import gettext as _
 
 import glv
 
-def get_support_keyboard() -> InlineKeyboardMarkup:
-   kb = [
+def get_support_keyboard(from_profile: bool = False) -> InlineKeyboardMarkup:
+    callback_data = 'help_from_profile' if from_profile else 'help'
+    kb = [
         [
             InlineKeyboardButton(text=_("button_vpn_not_working"), url=glv.config['VPN_NOT_WORKING_LINK'])
         ],
@@ -12,20 +13,21 @@ def get_support_keyboard() -> InlineKeyboardMarkup:
             InlineKeyboardButton(text=_("button_other_question"), url=glv.config['SUPPORT_LINK'])
         ],
         [
-            InlineKeyboardButton(text=_("button_back"), callback_data='help')
+            InlineKeyboardButton(text=_("button_back"), callback_data=callback_data)
         ],
     ]
    
-   return InlineKeyboardMarkup(inline_keyboard=kb)
+    return InlineKeyboardMarkup(inline_keyboard=kb)
 
-def get_reach_support_keyboard() -> InlineKeyboardMarkup:
-   kb = [
+def get_reach_support_keyboard(from_profile: bool = False) -> InlineKeyboardMarkup:
+    callback_data = 'support_from_profile' if from_profile else 'support'
+    kb = [
         [
             InlineKeyboardButton(text=_("button_support_failed"), url=glv.config['SUPPORT_LINK'])
         ],
         [
-            InlineKeyboardButton(text=_("button_back"), callback_data='support')
+            InlineKeyboardButton(text=_("button_back"), callback_data=callback_data)
         ],
     ]
    
-   return InlineKeyboardMarkup(inline_keyboard=kb)
+    return InlineKeyboardMarkup(inline_keyboard=kb)
