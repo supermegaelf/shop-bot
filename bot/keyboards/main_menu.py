92aecdf286af60d46aa9daf75642a2660cd5a289
supermegaelf <maksim.yurievich.makarov@gmail.com>
2025-11-22 00:41:36 +0300

    Feature: admin management menu

--- bot/keyboards/main_menu.py
+++ bot/keyboards/main_menu.py
@@ -2,7 +2,6 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.utils.i18n import gettext as _
 
 from utils import get_i18n_string
-import glv
 
 def get_main_menu_keyboard(user_id: int = None, lang=None) -> InlineKeyboardMarkup:
     kb = [
@@ -10,11 +9,6 @@ def get_main_menu_keyboard(user_id: int = None, lang=None) -> InlineKeyboardMark
             InlineKeyboardButton(text=get_i18n_str("button_vpn_access", lang), callback_data="vpn_access")
         ]
     ]
-
-    if user_id and int(user_id) in glv.config['ADMINS']:
-        kb.append([
-            InlineKeyboardButton(text=get_i18n_str("button_admin_management", lang), callback_data="admin_management")
-        ])
 
     return InlineKeyboardMarkup(inline_keyboard=kb)
 
