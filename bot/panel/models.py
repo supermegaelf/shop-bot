bae0563c9d8d3c8cc024d79357381e3ccdff15c2
supermegaelf <maksim.yurievich.makarov@gmail.com>
2025-12-03 15:57:28 +0300

    Refactor: remove marzban support

--- bot/panel/models.py
+++ bot/panel/models.py
@@ -25,13 +25,3 @@ class PanelProfile(BaseModel):
             expire=user.expire_at
         )
     
-    @classmethod
-    def from_marzban_profile(cls, marzban_profile: dict):
-        return cls(
-            username=marzban_profile.get('username'),
-            status=marzban_profile.get('status'),
-            subscription_url=glv.config['PANEL_GLOBAL'] + marzban_profile.get('subscription_url'),
-            used_traffic=marzban_profile.get('used_traffic'),
-            data_limit=marzban_profile.get('data_limit'),
-            expire=datetime.fromtimestamp(marzban_profile.get('expire')) if marzban_profile.get('expire') else None
-        )
--- bot/panel/panel_provider.py
+++ bot/panel/panel_provider.py
@@ -1,19 +1,8 @@
 from .panel import Panel
-from .marzban_panel import MarzbanPanel
 from .remnawave_panel import RemnawavePanel
 
-import glv
-
 def _init_panel() -> Panel:
-    panel_type = glv.config["PANEL_TYPE"]
-    panel_registry = {
-        "MARZBAN": MarzbanPanel,
-        "REMNAWAVE": RemnawavePanel
-    }
-    panel_class = panel_registry.get(panel_type)
-    if not panel_class:
-        raise ValueError(f"Unknown panel type: {panel_type}")
-    return panel_class()
+    return RemnawavePanel()
 
 panel = _init_panel()
 
